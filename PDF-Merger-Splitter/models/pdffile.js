73fdea149a7907d63854b98cb950b5421181812b
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 07:57:18 -0500

    Update pdffile.js

--- PDF-Merger-Splitter/models/pdffile.js
+++ PDF-Merger-Splitter/models/pdffile.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const PdfFileSchema = new mongoose.Schema({
+    filename: String,
+    createdAt: { type: Date, default: Date.now },
+    type: String // 'merged' or 'split'
+});
+
+module.exports = mongoose.model('PdfFile', PdfFileSchema);
 
