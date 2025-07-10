345bd339713f8323d33cebfb10f1a6e582a66912
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 07:59:42 -0500

    Update pdfroutes.js

--- PDF-Merger-Splitter/routes/pdfroutes.js
+++ PDF-Merger-Splitter/routes/pdfroutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { mergePDFs, splitPDF } = require('../controllers/pdfController');
+
+const upload = multer({ dest: 'uploads/' });
+
+router.post('/merge', upload.array('pdfs', 10), mergePDFs);
+router.post('/split', upload.single('pdf'), splitPDF);
+
+module.exports = router;
 
