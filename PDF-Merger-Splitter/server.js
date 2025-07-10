a9eaceb4473fe6eff62108da6d13e9bce3532501
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 08:03:29 -0500

    Update server.js

--- PDF-Merger-Splitter/server.js
+++ PDF-Merger-Splitter/server.js
@@ -8,4 +8,18 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 });
 const upload = multer({ storage });
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const pdfRoutes = require('./routes/pdfRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use('/api/pdf', pdfRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
