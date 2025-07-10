eed7adf96b2af7f575afe3e2fb676415719cbd4d
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 07:58:34 -0500

    Update server.js

--- PDF-Merger-Splitter/server.js
+++ PDF-Merger-Splitter/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+});
+const upload = multer({ storage });
 
